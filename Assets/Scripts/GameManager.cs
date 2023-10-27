903ab9e02dddb571af116c410a741cf13faac6a5
miandilan <miandilan@gmail.com>
2023-10-27 09:55:17 -0400

    Made gameManager singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,4 +7,33 @@ public class GameManager : MonoBehaviour
 {
     public int numEnemies;
     public int currentRound;
+
+    private static GameManager instance;//GameManager is a singleton with just 1 instance of it existing the whole game.
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                GameObject singleton = new GameObject("GameManager");
+                instance = singleton.AddComponent<GameManager>();
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
